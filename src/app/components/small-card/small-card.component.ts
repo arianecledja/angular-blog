d3a7823c1396934594ecb9c7cc4c65674104291f
Ariane Santos <ariane225040@gmail.com>
2023-09-28 19:41:37 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -12,6 +12,9 @@ export class SmallCardComponent implements OnInit {
   @Input()
   cardTitle: string = ""
 
+  @Input()
+  Id: string = "0"
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    'id': "1",
+    'title': "NOVO HOMEM DE FERRO EM 3D",
+    'description': "Marvel Studios anuncia novo filme do Homem de Ferro, confira",
+    'photoCover': "https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q95/wp-content/uploads/2020/09/legiao_M_2tqY78ozVp.jpg.webp"
+  },
+  {
+    'id': "2",
+    'title': "NOVA SÉRIE ANUNCIADA NO DISNEY +",
+    'description': "lorem ipsum",
+    'photoCover': "https://legadodamarvel.com.br/wp-content/uploads/2022/04/series-da-marvel-nao-trouxeram-numeros-tao-bons-para-o-disney-plus-legadodamarvel.jpg"
+  }
+]
